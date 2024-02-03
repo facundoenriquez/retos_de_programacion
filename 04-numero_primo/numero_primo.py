eddb47a2f30467b3335cd464abc7eafde4474e7f
facundo enriquez <facundo.tomas.enriquez@gmail.com>
2024-02-03 13:38:32 -0300

    primal_numbers

--- 04-numero_primo/numero_primo.py
+++ 04-numero_primo/numero_primo.py
@@ -1,13 +1,20 @@
 numbers = range(1, 101)
 primal_numbers = []
 
+
 def isPrimal(num):
+    count = 0
     for i in numbers:
-        if i > 1 and 
-    return True
+        if num % i == 0 and i > 1 and num > 1:
+            count += 1    
+    if count == 1:
+        return True
+    return False
 
 
 for num in numbers:
-    if isPrimal(num):
+    primal = isPrimal(num)
+    if primal == True:
         primal_numbers.append(num)
 
+print(primal_numbers)
